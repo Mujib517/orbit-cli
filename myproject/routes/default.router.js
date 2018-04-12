2521905e3d8143697e68d6dbb83431f4f2d9ebbc
Mujib <mujib517@gmail.com>
2018-04-12 19:04:28 +0530

    Delete unused files

--- myproject/routes/default.router.js
+++
@@ -1,7 +0,0 @@
-var router = require('express').Router();
-var defaultCtrl = require("../controllers/default.ctrl");
-
-router.get('/', defaultCtrl.get);
-router.get('/health', defaultCtrl.health);
-
-module.exports = router;
