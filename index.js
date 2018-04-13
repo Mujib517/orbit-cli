9330c5a174b6177508ef5767ce4a69c47bc08ed1
Mujib517 <Mujib517@gmail.com>
2018-04-13 12:16:02 +0530

    Fixes

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ if (process.argv.length < 3) {
 }
 
 var projectName = process.argv[2];
-var location = path.dirname(process.argv[1]);
+var location = path.resolve('./');
 var projectLocation = path.join(location, projectName);
 
 var config = {
-    cwd: location,
+    cwd:path.dirname(process.argv[1]),
     projectLocation: projectLocation,
-    projectName: projectName
+    projectName: projectName,
 };
 
 var helpers = require('./helpers')(config);
 helpers.createProject();
-helpers.installDependencies();
 
