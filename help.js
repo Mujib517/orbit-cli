613d531faeb2d6795803a9aa00102ac3bcf833b7
Mujib517 <Mujib517@gmail.com>
2018-04-24 12:52:42 +0530

    Updated Help

--- help.js
+++ help.js
@@ -1,5 +1,6 @@
 module.exports = function showHelp() {
     console.log("n, new \t\t", "To create a new project.\t Usage: orbit new project-name");
     console.log("r, route \t\t", "To create a new router file.\t Usage: orbit route route-name");
+    console.log("mw, middleware \t\t", "To create a new middleware.\t Usage: orbit middleware middleware-name");
     console.log("help, --help \t\t", "To get help.\t Usage: orbit help");
 }
